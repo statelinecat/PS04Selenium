90aff446abc0e1d15bd808c12796b251a845d552
statelinecat <statelinecat@gmail.com>
2024-08-16 04:45:21 +0300

    add while

--- main.py
+++ main.py
@@ -16,14 +16,11 @@
 
 
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys
 import time
+import random
+
 
 browser = webdriver.Firefox()
 browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
@@ -33,25 +30,47 @@ action1 = input("\nВведите запрос: ")
 search_box = browser.find_element(By.ID, "searchInput")
 search_box.send_keys(action1)
 search_box.send_keys(Keys.RETURN)
+time.sleep(10)
 a = browser.find_element(By.LINK_TEXT, action1)
-#Добавляем клик на элемент
 a.click()
 
-action2 = input("\nВыберите действие:\n"
-               "1. Листать параграфы текущей статьи\n"
-               "2. Перейти на одну из связанных страниц\n"
-               "3. Выйти\n")
+while True:
+    action2 = input("\nВыберите действие:\n"
+                "1. Листать параграфы текущей статьи\n"
+                "2. Перейти на одну из связанных страниц\n"
+                "3. Выйти\n")
 
-if action2 == "1":
-    paragraphs = browser.find_elements(By.TAG_NAME, "p")
-    for paragraph in paragraphs:
-        print(paragraph.text)
-        input("\nНажмите Enter для продолжения...")
+    if action2 == "1":
+        paragraphs = browser.find_elements(By.TAG_NAME, "p")
+        for paragraph in paragraphs:
+            print(paragraph.text)
+            per = input("\nНажмите Enter для продолжения или 0 для выхода: ")
+            if per == "0":
+                break
+
+    elif action2 == "2":
+        hatnotes = []
+        links = browser.find_elements(By.TAG_NAME, "div")
+        for link in links:
+            cl = link.get_attribute("class")
+            if cl == "hatnote navigation-not-searchable":
+                hatnotes.append(link)
+        print(hatnotes)
+        if len(hatnotes) == 0:
+            print("Нет связанных страниц")
+        else:
+            hatnote = random.choice(hatnotes)
+            link1 = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+            browser.get(link1)
+
+    elif action2 == "3":
+        time.sleep(2)
+        browser.quit()
+        exit()
 
 
 
 
 
-time.sleep(10)
-browser.quit()
+
 
